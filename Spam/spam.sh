c50b0bc39a6b4c7a869cbbed1d68d280a9dc9a19
MrS3V3NT33N <44054573+MrS3V3NT33N@users.noreply.github.com>
2018-10-14 06:51:27 +0000

    Add files via upload

---
+++ Spam/spam.sh
@@ -0,0 +1,133 @@
+clear
+toilet -f mono12 -F gay "S3V3NT33N SPAM"
+sleep 1
+echo "\033[34;1m Macam-macam tool untuk spam"
+sleep 1
+echo "\033[38;1m ------------------------------------------------------------------"|lolcat
+echo "\033[32;1m #jangan lupa bahagia"
+echo "\033[31;1m author: MR S3V3NT33N"
+echo "\033[34;1m subscribe my youtube channel : https://www.youtube.com/channel/UCyDJgv9x0kTGWn7cO9MEAAA"
+echo "\033[38;1m ******************************************************************"|lolcat
+
+date|lolcat
+echo ""
+sleep 1
+echo "\033[31;1m spam yang tersedia"
+echo "1. Tokopedia"
+echo "2. Telkomsel"
+echo "3. kfc"
+echo "4. PHD"
+echo "5. zipay"
+echo "6. hooq"
+echo "7. whiskas"
+echo "8. mataharimall"
+echo "9. jdid"
+echo "0. keluar"
+echo "\033[32;1m17. Informasi Tool Ini"
+echo "\033[37;1m Pilih Angka:"
+read ezz
+if [ $ezz = 1 ] || [ $ezz = 1 ]
+then
+clear
+echo "\033[34;1m"
+toilet -f slant --gay "tokopedia"
+php tokped.php
+fi
+
+if
+[ $ezz = 2 ] || [ $ezz = 2 ]
+then
+clear
+toilet -f mono -F metal "T-sel"
+echo "\033[31;1m"
+php 2.php 
+fi
+
+if [ $ezz = 3 ] || [ $ezz = 3 ]
+then
+clear
+echo "\033[31;1m"
+figlet "kfc"
+php kfc.php
+fi
+
+if [ $ezz = 4 ] || [ $ezz = 4 ]
+then
+clear
+toilet -f mono12 -F gay "phd"
+echo "\033[30;1m"
+php phd.php
+fi
+
+if
+[ $ezz = 5 ] || [ $ezz = 5 ]
+then
+clear
+toilet "zipay"
+echo "\033[33;1m"
+php zipay.php
+fi
+
+if
+[ $ezz = 6 ] || [ $ezz = 6 ]
+then
+clear
+toilet -f mono12 -F gay "hooq"
+echo "\033[31;1m"
+php hooq.php
+fi
+
+if [ $ezz = 7 ] || [ $ezz = 7 ]
+then
+clear
+echo "\033[36;1m"
+figlet "whiskas"
+php whiskas.php
+fi
+
+if [ $ezz = 8 ] || [ $ezz = 8 ]
+then
+clear
+echo "\033[31;1m"
+toilet "mataharimall"
+php mataharimall.php
+fi
+
+if [ $ezz = 9 ] || [ $ezz = 9 ]
+then
+clear
+echo "\033[37;1m"
+figlet "jdid"
+php jdid.php
+fi
+
+if
+[ $ezz = 17 ] || [ $ezz = 17 ]
+then
+clear
+toilet -f slant --gay "S3V3NT33N SPAM"
+echo "\033[31;1mNama tools: S3V3NT33N SPAM"
+sleep 1
+echo "\033[33;1mKarya: MR.S3V3NT33N"
+sleep 1
+echo "\033[32;1mVersi: v1"
+sleep 1
+echo "\033[31;1m Subscribe my youtube channel: "
+echo "\033[36;1m"
+echo "https://www.youtube.com/channel/UCyDJgv9x0kTGWn7cO9MEAAA"
+sleep 10
+echo "\033[30;1m tunggu 10 detik"
+sleep 10
+sh MrS3V3NT33N.sh
+fi
+
+
+if
+[ $ezz = 0 ] || [ $ezz = 0 ]
+then
+echo "\033[31;1m Keluar"
+sleep 1
+echo "\033[32;1m SEMOGA HARIMU MENYENANGKAN :)"
+sleep 1
+fi
+
